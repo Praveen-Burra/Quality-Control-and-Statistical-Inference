bd156de005a58cf00bed4e85b2b69b99764b05b7
PRAVEEN BURRA <40871952+Praveen-Burra@users.noreply.github.com>
2019-08-25 23:36:05 -0500

    Update HistTAI.R

--- 2.Trait Psychometric Data/HistTAI.R
+++ 2.Trait Psychometric Data/HistTAI.R
@@ -1,4 +1,4 @@
-TAI_scores<-read.csv("C:\\Users\\Swetcha\\Desktop\\Stats\\project_new\\Methodist microsurgery with output\\tai_scores.txt",header=FALSE)
+TAI_scores<-read.csv("C:\\Users\\Praveen\\Desktop\\Stats\\project_new\\Methodist microsurgery with output\\tai_scores.txt",header=FALSE)
 #TAI_scores
 hist(TAI_scores$V2,xlim=c(20,80),col=c("green","green","green","green","orange","orange"),main="Histogram for TAI scores",xlab="Scores")
 legend("topright",c("Normal","Higher"),col=c("green","orange"),pch=15)
